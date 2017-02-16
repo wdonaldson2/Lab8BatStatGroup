00d6cd48019169bcb5c36b4ef7ddf41ee5924deb
William D <hardworkday59@gmail.com>
2017-02-15 20:17:18 -0500

    first commit

--- src/lab8A.java
+++ src/lab8A.java
@@ -12,7 +12,6 @@ public class lab8A {
         int atBat = 0;
         int baseSum = 0;
 
-
         System.out.println("How many batters are in the game? ");
         numBatters = scan.nextInt();
 
@@ -22,54 +21,39 @@ public class lab8A {
 
         do {
             //loop through the batters (rows)
-            // for (int row = 0; row < numBatters; row++) {
             int[][] batStats = new int[numBatters][atBat];
-            for (int row = 0; row < batStats.length; row++) {
+            for (int row = 0; row < batStats.length; row++) {//this loop get numbatters
 
                 int sum = 0;
-                for (int col = 0; col < batStats[row].length; col++) {
-
-                    //int total = 0;
+                for (int col = 0; col < batStats[row].length; col++) {// atbats loop
 
                     System.out.println("Enter Player's number of at Bat's: " + (col + 1));
 
-                    //total = scan.nextInt();
-
                     batStats[row][col] = scan.nextInt();
 
-                    //sum = batStats[row][col];
                 }
 
-                //System.out.println(batStats);
-                //int [] numBatter = new int[atBat];
+                    int bases = 0;
+                    int hits = 0;
 
-                System.out.println(sum / batStats[row].length);
+                    for (int col = 0; col < batStats[row].length; col++) {//calc statistics
 
-                for (int num = 0; row < batStats.length; row++) {
+                        bases += batStats[row][col];
+                        if (batStats[row][col] > 0) {
+                            hits++;
+                        }
 
-                  int bases = 0;
-                  int hits = 0;
+                    }
 
-                for (int col = 0; col < batStats[row].length; col++) {
+                double batAverg = (double) hits / batStats[row].length;
+                System.out.printf("For batter %d, Batting Average: %.3f", (row + 1), batAverg);
 
-                  bases += batStats[row][col];
-                  if (batStats[row][col] > 0) {
-                  hits++;
+                double slugPercentage = (double) bases / batStats[row].length;
+                System.out.printf("\tBatting Average: %.3f\n", slugPercentage);
 
-                 }
-
-                 }
-                  double batAverg = (double) hits / batStats[row].length;
-                 System.out.printf("For batter %d, Batting Average: %.3f", (row +1), batAverg);
-
-                  double slugPercentage = (double) bases / batStats[row].length;
-                 System.out.printf("\tBatting Average: %.3f\n", slugPercentage);
-
-                 System.out.println("Another batter? (y/n): ");
                 }
-                }
-
-    }while (input.next().equalsIgnoreCase("Y")) ;
+            System.out.println("Another batter? (y/n): ");
+    } while (input.next().equalsIgnoreCase("Y")) ;
         input.close ();
     }
 }
